334a1aa9ae28aae465a5a0eb47fdbfbde290e732
Mohamed <Mohamed.cherif@esen.tn>
2024-07-13 10:17:49 +0100

    final

--- ExamTPF_Mohamed_Cherif/main.py
+++ ExamTPF_Mohamed_Cherif/main.py
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 from add import *
 x=int(input('Donner x'))
 y=int(input('Donner y'))
-print ('La somme de ',x,'et',y,'vaut',add(x,y))
-=======
-x=int(input('Donner x: '))
-y=int(input('Donner y: '))
-print('LA somme de ',x,'et',y'vaut',x+y)
->>>>>>> master
-
+s=add(x,y)
+print ('La somme de ',x,'et',y,'vaut',s)
